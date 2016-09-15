099acae3182fe1f226ca23210f585105a6d2c26d
Prantar Bhowmik <prantarbhowmik@yahoo.com>
2016-09-15 16:33:15 -0400

    License and minor changes

--- quickstart.py
+++ quickstart.py
@@ -29,7 +29,7 @@ except ImportError:
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/gmail-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+SCOPES = 'https://mail.google.com/'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Gmail API Python Quickstart'
 
